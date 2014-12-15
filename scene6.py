d277d8d7fd36047afa419726bcc5c0ecb84665ac
Maxime Tournier <maxime.tournier@brain.riken.jp>
2014-12-15 15:42:49 +0900

    doc

--- scene6.py
+++ scene6.py
@@ -9,6 +9,10 @@ def createScene( node ):
                   velocity = [0, 5, 0,
                               0, 1, 0],
                   mesh = 'mesh/torus.obj')
+
+    # key = value arguments are passed as a dictionary to the toolbox.rigid function
+    
+    # => { 'velocity': [0, 5, 0 ... ], 'mesh': 'mesh/torus.obj' }
     
     style = node.createObject('VisualStyle')
     style.displayFlags = 'showBehavior showCollision showMapping hideVisual'
@@ -20,5 +24,6 @@ def createScene( node ):
     node.animate = 1
 
     return 0
+
 
 
--- scene7.py
+++ scene7.py
@@ -10,13 +10,14 @@ def createScene( node ):
                               0, 1, 0],
                   mesh = 'mesh/torus.obj')
 
+    # forces sofa to load a plugin before continuing
     plugin = node.createObject('RequiredPlugin',
                                pluginName = 'Compliant')
-
     
     style = node.createObject('VisualStyle')
     style.displayFlags = 'showBehavior showCollision showMapping hideVisual'
 
+    # these are from the Compliant plugin
     ode = node.createObject('CompliantImplicitSolver')
     num = node.createObject('SequentialSolver')
 
